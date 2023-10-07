08f700748d9ecff49ad928b25b06c55cf0c10039
Mahmoud Abulmagd <me@abulmagd.dev>
2023-10-07 13:40:00 +0300

    - Push web build

--- lib/resources/assets.dart
+++ lib/resources/assets.dart
@@ -1,6 +1,6 @@
 abstract final class Paths {
-  static const svg = 'assets/svg';
-  static const images = 'assets/images';
+  static const svg = '/svg';
+  static const images = '/images';
 }
 
 abstract final class Assets {

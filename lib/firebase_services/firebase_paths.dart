33934d5d0610b8a706cb021023499ec1f3d70b8a
Mahmoud Abulmagd <me@abulmagd.dev>
2023-12-06 20:48:44 +0200

    - Add folder paths for firebase storage - Documentation for [FirebasePaths] fields - Add path for firebase storage main bucket

--- lib/firebase_services/firebase_paths.dart
+++ lib/firebase_services/firebase_paths.dart
@@ -1,13 +1,23 @@
 enum FirebasePaths {
-  blogs('blogs/'),
-  clientFeedbacks('client_feedbacks/'),
-  events('events/'),
+  /// Storage Instructions: Upload directly
+  blogs('blogs/', ''),
+
+  /// Storage Instructions: client_name/
+  clientFeedbacks('client_feedbacks/', ''),
+
+  /// Storage Instructions: event_name/
+  events('events/', ''),
   offers('offers/'),
+
+  /// Storage Instructions: project_name/
   projects('projects/', ''),
-  purchasableServices('purchasable_services/'),
+
+  /// Storage Instructions: Upload directly
+  purchasableServices('purchasable_services/', ''),
   services('services/'),
   staff('staff/'),
-  technologies('technologies/');
+  technologies('technologies/', ''),
+  storageBucket('', 'gs://zognest-website.appspot.com/');
 
   final String firestorePath;
   final String? storagePath;

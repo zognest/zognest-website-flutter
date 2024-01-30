2927960793f0137c43dab1ded886d3966182276b
Mahmoud Abulmagd <me@abulmagd.dev>
2024-01-30 21:56:47 +0200

    Adjustments

--- lib/shared/widgets/footer.dart
+++ lib/shared/widgets/footer.dart
@@ -37,103 +37,138 @@ class _FooterState extends State<Footer> {
           ],
         ),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+      child: Stack(
         children: [
-          const Divider(),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: Constants.horizontalPadding,
-              vertical: Spacing.l24,
-            ),
+          Align(
+            alignment: Alignment.bottomCenter,
             child: Column(
               children: [
-                const ZognestLogo(
-                  iconOnly: true,
-                  size: 80,
-                ),
-                const SizedBox(height: Spacing.s12),
-                InkWell(
-                  onTap: () {},
-                  onHover: (over) {
-                    setState(() => hovered = over);
-                  },
-                  overlayColor:
-                      const MaterialStatePropertyAll(Palette.transparent),
-                  child: Text(
-                    Strings.zognestMail,
-                    style: theme.textTheme.headlineLarge?.copyWith(
-                      fontWeight: FontWeight.w700,
-                      fontSize: Responsive.isDesktop(context) ? 70 : 20,
-                      color: hovered
-                          ? theme.primaryColor
-                          : const Color(0xffAEB2BA),
-                    ),
+                const SizedBox(height: 24),
+                const Divider(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: Constants.horizontalPadding,
+                    vertical: Spacing.l24,
                   ),
-                ),
-                const SizedBox(height: Spacing.l24),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  textBaseline: TextBaseline.alphabetic,
-                  children: [
-                    Expanded(
-                      child: AutoSizeText(
-                        Strings.zognest.toUpperCase(),
-                        maxLines: 1,
-                        textAlign: TextAlign.center,
-                        style: theme.textTheme.displayLarge?.copyWith(
-                          fontFamily: 'SF Pro Rounded',
-                          fontWeight: FontWeight.w900,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const ZognestLogo(
+                        iconOnly: true,
+                        size: 80,
+                      ),
+                      const SizedBox(height: Spacing.s12),
+                      InkWell(
+                        onTap: () {},
+                        onHover: (over) {
+                          setState(() => hovered = over);
+                        },
+                        overlayColor:
+                            const MaterialStatePropertyAll(Palette.transparent),
+                        child: Text(
+                          Strings.zognestMail,
+                          style: theme.textTheme.headlineLarge?.copyWith(
+                            fontWeight: FontWeight.w700,
+                            fontSize: Responsive.isDesktop(context) ? 70 : 20,
+                            color: hovered
+                                ? theme.primaryColor
+                                : const Color(0xffAEB2BA),
+                          ),
                         ),
                       ),
-                    ),
-                    if (Responsive.isDesktop(context))
-                      CircleButton(
-                        onTap: widget.onTabUp,
-                        radius: Responsive.isDesktop(context)
-                            ? Spacing.l32
-                            : Spacing.m16,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              height: Constants.circleButtonRadius,
-                              decoration: BoxDecoration(
-                                color: theme.primaryColor,
-                                shape: BoxShape.circle,
+                      const SizedBox(height: Spacing.l24),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        textBaseline: TextBaseline.alphabetic,
+                        children: [
+                          Expanded(
+                            child: AutoSizeText(
+                              Strings.zognest.toUpperCase(),
+                              maxLines: 1,
+                              textAlign: TextAlign.center,
+                              style: theme.textTheme.displayLarge?.copyWith(
+                                fontFamily: 'SF Pro Rounded',
+                                fontWeight: FontWeight.w900,
                               ),
                             ),
-                            const SizedBox(height: Spacing.s4),
-                            SvgPicture.asset(
-                              Assets.upArrow,
-                              height: Responsive.isDesktop(context)
-                                  ? Spacing.m20
-                                  : Spacing.s12,
+                          ),
+                          if (Responsive.isDesktop(context))
+                            CircleButton(
+                              onTap: widget.onTabUp,
+                              radius: Responsive.isDesktop(context)
+                                  ? Spacing.l32
+                                  : Spacing.m16,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    height: Constants.circleButtonRadius,
+                                    decoration: BoxDecoration(
+                                      color: theme.primaryColor,
+                                      shape: BoxShape.circle,
+                                    ),
+                                  ),
+                                  const SizedBox(height: Spacing.s4),
+                                  SvgPicture.asset(
+                                    Assets.upArrow,
+                                    height: Responsive.isDesktop(context)
+                                        ? Spacing.m20
+                                        : Spacing.s12,
+                                  ),
+                                ],
+                              ),
                             ),
-                          ],
+                        ],
+                      ),
+                      const SizedBox(height: Spacing.l24),
+                      Wrap(
+                        spacing: 4,
+                        runSpacing: 4,
+                        children: SocialButtons.values
+                            .map((button) => SocialButton(button: button))
+                            .toList(),
+                      ),
+                      const SizedBox(height: Spacing.l24),
+                      Text(
+                        Strings.copyRights,
+                        textAlign: TextAlign.center,
+                        style: theme.textTheme.labelMedium?.copyWith(
+                          fontWeight: FontWeight.w500,
                         ),
                       ),
-                  ],
-                ),
-                const SizedBox(height: Spacing.l24),
-                Wrap(
-                  spacing: 4,
-                  runSpacing: 4,
-                  children: SocialButtons.values
-                      .map((button) => SocialButton(button: button))
-                      .toList(),
-                ),
-                const SizedBox(height: Spacing.l24),
-                Text(
-                  Strings.copyRights,
-                  textAlign: TextAlign.center,
-                  style: theme.textTheme.labelMedium?.copyWith(
-                    fontWeight: FontWeight.w500,
+                    ],
                   ),
                 ),
               ],
             ),
           ),
+          if (!Responsive.isDesktop(context)) ...[
+            Positioned(
+              right: Spacing.l32,
+              top: 0,
+              child: CircleButton(
+                onTap: widget.onTabUp,
+                radius: Spacing.l32,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: Spacing.s4,
+                      decoration: BoxDecoration(
+                        color: theme.primaryColor,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    const SizedBox(height: Spacing.s4),
+                    SvgPicture.asset(
+                      Assets.upArrow,
+                      height: Spacing.m20,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ],
       ),
     );
